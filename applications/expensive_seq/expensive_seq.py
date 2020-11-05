3cee32ab704fd8b92b3baba257d8d0e5dd4887c8
Jeff Breig <jjbreig@gmail.com>
2020-11-04 21:29:44 -0500

    finished no_dups, should be O(n)

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,11 @@
 # Your code here
-
+import math
+cache = {}
 
 def expensive_seq(x, y, z):
     # Your code here
-
+     if x <= 0: y + z
+     if x >  0: exp(x-1,y+1,z) + exps(x-2,y+2,z*2) + exps(x-3,y+3,z*3)
 
 
 if __name__ == "__main__":
--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,17 @@
 def no_dups(s):
     # Your code here
-
+    if s == "":
+        return ""
+    new_string = []
+    split_string = s.split(' ')
+    for word in split_string:
+        if word not in new_string:
+            new_string.append(word)
+    
+    sep = " "
+    final_string = sep.join(new_string)
+    print("This is final string: ", final_string)
+    return final_string
 
 
 if __name__ == "__main__":
