03a0a4f4d3e88f77734a90cd48200cd4eb719c16
Jeff Breig <jjbreig@gmail.com>
2020-11-05 14:48:00 -0500

    beginning markov chains

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,8 +1,9 @@
 import random
 
 # Read in all the words in one go
-with open("input.txt") as f:
+with open("applications\markov\input.txt") as f:
     words = f.read()
+    
 
 # TODO: analyze which words can follow other words
 # Your code here
