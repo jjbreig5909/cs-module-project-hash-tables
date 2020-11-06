748ec50d84c270e09d8419930a05e9474d34845f
Jeff Breig <jjbreig@gmail.com>
2020-11-05 21:58:26 -0500

    found all combos

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -5,11 +5,30 @@ f(a) + f(b) = f(c) - f(d)
 
 #q = set(range(1, 10))
 #q = set(range(1, 200))
-q = (1, 3, 4, 7, 12)
 
+q = (1, 3, 4, 7, 12)
+results = []
 
 def f(x):
     return x * 4 + 6
 
 # Your code here
+import itertools
+answers = []
+
+for num in q:
+    answers.append(f(num))
+
+arr1 = [1, 3, 4, 7, 12]
+arr2 = []
+
+potential_combinations = itertools.product(q, repeat=4)
+
+def find_combos(combos):
+    for item in combos:
+       if item[0]+item[1] == item[2]-item[3]:
+           results.append([item[0], item[1], item[2],item[3]])
+
+find_combos(potential_combinations)
+print(results)
 
