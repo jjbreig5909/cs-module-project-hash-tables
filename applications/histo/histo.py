91cd272a5e9b17e486abb1bf9e07e9c48bdc9964
Jeff Breig <jjbreig@gmail.com>
2020-11-05 19:31:27 -0500

    finished word histogram

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -2,7 +2,22 @@
 with open('applications/histo/robin.txt') as f:
     words = f.read()
 
+word_cache = {}
 words = words.lower()
-
+exclude = set('" : ; , . - + = / \ | [ ] { } ( ) * ^ &')
 word_array = words.split (' ')
 print(word_array)
+
+for word in word_array:
+    if word not in exclude:
+
+        if word in word_cache:
+            word_cache[word] += "#"
+        
+        if word not in word_cache:
+            word_cache[word] = "#"
+       
+
+sorted_words = sorted(word_cache.items(), key=lambda x: x[1], reverse=True)
+
+print(sorted_words)
